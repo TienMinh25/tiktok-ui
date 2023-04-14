56fae0ec075a2ac2e0fdaeb305ad3c1cda4e4f00
Tien Minh <tienminh@Tiens-MacBook-Air.local>
2023-04-14 17:19:59 +0700

    Giai quyet van de nhieu file index.js

---
+++ src/components/AccountItem/AccountItem.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './AccountItem.module.scss';
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+import Image from '~/components/Image';
+const cx = classNames.bind(styles);
+
+function AccountItem({ data }) {
+    return (
+        <Link className={cx('wrapper')} to={`/@${data.nickname}`}>
+            <Image src={data.avatar} className={cx('search-avatar')} alt={data.full_name} />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>{data.full_name} </span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCircleCheck} />}
+                </h4>
+                <span className={cx('username')}>{data.nickname}</span>
+            </div>
+        </Link>
+    );
+}
+
+AccountItem.propTypes = {
+    data: PropTypes.object.isRequired,
+};
+
+export default AccountItem;
---
+++ src/layouts/components/Header/Header.js
@@ -0,0 +1,152 @@
+import config from '~/config';
+
+import styles from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+import { Link } from 'react-router-dom';
+import Button from '~/components/Button';
+import classNames from 'classnames/bind';
+import images from '~/assets/images';
+import Menu from '~/components/Popper/Menu';
+import {
+    FeedbackIcon,
+    GetCoinIcon,
+    InboxIcon,
+    KeyboardIcon,
+    LanguageIcon,
+    LogoutIcon,
+    MessageIcon,
+    SettingIcon,
+    UploadIcon,
+    UserIcon,
+} from '~/components/Icons';
+import Image from '~/components/Image';
+import Search from '../Search';
+
+const cx = classNames.bind(styles); // styles cx la 1 doi tuong
+const MENU_ITEMS = [
+    {
+        icon: <LanguageIcon />,
+        title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'language',
+                    code: 'en',
+                    title: 'English',
+                },
+                {
+                    type: 'language',
+                    code: 'vi',
+                    title: 'Vietnamese',
+                },
+            ],
+        },
+    },
+    {
+        icon: <FeedbackIcon />,
+        title: 'Feedback and help',
+        to: '/feedback',
+    },
+    {
+        icon: <KeyboardIcon />,
+        title: 'Keyboard shortcuts',
+    },
+];
+function Header() {
+    const currentUser = true;
+
+    //Handle logic
+    const handleMenuChange = (menuItem) => {
+        console.log(menuItem);
+    };
+
+    const userMenu = [
+        {
+            icon: <UserIcon />,
+            title: 'View profile',
+            to: '@quynhhuong',
+        },
+        {
+            icon: <GetCoinIcon />,
+            title: 'Get coins',
+            to: '/coin',
+        },
+        {
+            icon: <SettingIcon />,
+            title: 'Settings',
+            to: '/setting',
+        },
+        ...MENU_ITEMS,
+        {
+            icon: <LogoutIcon />,
+            title: 'Log out',
+            to: '/logout',
+            separate: true,
+        },
+    ];
+
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <Link to={config.routes.home} className={cx('logo-link')}>
+                        <img src={images.logo} alt="tiktok" />
+                    </Link>
+                </div>
+
+                {/* Header Search */}
+                <Search />
+
+                <div className={cx('action')}>
+                    {currentUser ? (
+                        <>
+                            <Tippy delay={[0, 200]} content="Upload" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <UploadIcon width={'2.6rem'} height={'2.6rem'} />
+                                </button>
+                            </Tippy>
+                            <Tippy delay={[0, 200]} content="Messages" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <MessageIcon width={'2.6rem'} height={'2.6rem'} />
+                                </button>
+                            </Tippy>
+                            <Tippy delay={[0, 200]} content="Notifications" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <InboxIcon />
+                                </button>
+                            </Tippy>
+                        </>
+                    ) : (
+                        <>
+                            <Button text>Upload</Button>
+                            <Button primary className={cx('custom-login')}>
+                                Log in
+                            </Button>
+                        </>
+                    )}
+
+                    <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
+                        {currentUser ? (
+                            <Image
+                                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/fcefe8d678a42366c170508cdf404aac~c5_100x100.jpeg?biz_tag=tiktok_user.user_cover&x-expires=1681441200&x-signature=%2Fe7heDQCX9iPIqH7oU888weLcb0%3D"
+                                className={cx('user-avatar')}
+                                alt="Nguyen Van A"
+                                fallback="https://scontent.fhan14-4.fna.fbcdn.net/v/t39.30808-1/335285037_2381428085350476_1092623240281100112_n.jpg?stp=c0.85.480.480a_dst-jpg_p480x480&_nc_cat=109&ccb=1-7&_nc_sid=7206a8&_nc_ohc=JAmfCBZMzZ8AX_wPWB9&_nc_ht=scontent.fhan14-4.fna&oh=00_AfCn0lYzSU77U7fjOfc472FvM3OSx8MqgTjVniA1D5ytcw&oe=643C1141"
+                            />
+                        ) : (
+                            <button className={cx('more-btn')}>
+                                <FontAwesomeIcon icon={faEllipsisVertical} />
+                            </button>
+                        )}
+                    </Menu>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
