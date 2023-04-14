4362cba00bb9a23caa7ca96c1b0d5fda94697463
Tien Minh <tienminh@Tiens-MacBook-Air.local>
2023-04-14 17:03:06 +0700

    Ap dung thu vien PropTypes vao du an

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './AccountItem.module.scss';
 import classNames from 'classnames/bind';
@@ -20,5 +21,9 @@ function AccountItem({ data }) {
         </Link>
     );
 }
+
+AccountItem.propTypes = {
+    data: PropTypes.object.isRequired,
+};
 
 export default AccountItem;
